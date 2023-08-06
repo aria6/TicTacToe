981527e421d9d168fa61cb54826e41b4d9fdb200
aria6 <dian.ariawal@gmail.com>
2023-08-06 13:04:31 +0700

    5.7 Menggabungkan Komponen Sesuai Dengan Desain

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+import Button from './core-ui/Button';
+import Board from './components/Board';
 
 class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <StatusBar style='auto' />
-        <Text>TIC TAC TOE</Text>
+        <View style={styles.header}>
+          <Text style={styles.title}>TIC TAC TOE</Text>
+          <Text style={styles.subtitle}>Sekarang Giliran Jalan</Text>
+          <Text style={styles.subtitle}>X</Text>
+        </View>
+        <Board />
+        <View style={styles.footer}>
+          <Button text='Prev' onPress={() => {}} />
+          <Button text='Reset' onPress={() => {}} />
+          <Button text='Next' onPress={() => {}} />
+        </View>
       </View>
     );
   }
@@ -18,7 +32,21 @@ let styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 25,
+  },
+  footer: {
+    flexDirection: 'row',
   },
 });
 
